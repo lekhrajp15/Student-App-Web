4b13b655af5dd148d259c3e11835c1e169cd0472
lekhraj <lekhraj.p15@gmail.com>
2024-06-29 17:56:26 +0530

    Code Updated

--- Test/conftest.py
+++ Test/conftest.py
@@ -1,8 +1,12 @@
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+driver = None
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Type of browser. Default is chrome.")
@@ -13,6 +17,7 @@ def browser(request):
 
 @pytest.fixture
 def setup(request, browser):
+    global driver
     if browser == "chrome":
         chrome_options = ChromeOptions()
         chrome_options.add_argument('--disable-notifications')
@@ -35,4 +40,32 @@ def setup(request, browser):
 
     yield driver
     driver.quit()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
+
 
